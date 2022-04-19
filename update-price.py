218d9901b30cee7ac790763c7a21b110d0a02f3d
asksmruti <asksmruti@gmail.com>
2022-04-19 12:20:02 +0200

    First commit

---
+++ update-price.py
@@ -0,0 +1,20 @@
+import boto3
+import os
+
+
+def lambda_handler(event, context):
+    db_host = os.environ['db_host']
+    client = boto3.resource('dynamodb')
+    table = client.Table(db_host)
+    response = table.put_item(
+        Item={
+
+            'productId': event['productId'],
+            'price': event['price']
+        }
+    )
+
+    return {
+        'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
+        'body': 'Product ' + event['productId'] + ' updated'
+    }
